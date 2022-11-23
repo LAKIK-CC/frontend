d700085577d6beecec19ba10150cbf8b8ea064df
Rafli Bangsawan <rafli.bangsawan@ui.ac.id>
2022-11-23 23:08:59 +0700

    fixed token expiry

--- src/routes/createKamar/CreateKamar.js
+++ src/routes/createKamar/CreateKamar.js
@@ -7,7 +7,7 @@ import * as yup from "yup";
 import axios from 'axios';
 import BASE_URL from '../../config/api/Constant.js';
 import ROUTE from '../../config/api/Route.js';
-import { getUserToken } from '../../config/api/Auth.js';
+import { getUserAccessToken } from '../../config/api/Auth.js';
 
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
@@ -40,7 +40,7 @@ const CreateKamar = () => {
     try {
       await axios.post(`${BASE_URL}/v1/kamar`, res, {
         headers: {
-          Authorization: `Bearer ${getUserToken()}`
+          Authorization: `Bearer ${getUserAccessToken()}`
         }
       })
       .then((response) => {
